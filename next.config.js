07abf2210355ac16c6b7d7e75845e7d260f10996
matinsama30-cpu <matinsama30@gmail.com>
2026-08-07 01:57:35 +0330

    Add initial Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,24 @@
+<function_calls>
+
+<invoke name="file_new">
+
+<parameter name="path">/tmp/vercel-setup/next.config.js</parameter>
+
+<parameter name="content">/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+
+  reactStrictMode: true,
+
+  swcMinify: true,
+
+}
+
+
+module.exports = nextConfig
+
+</parameter>
+
+</invoke>
+
+</function_calls>
