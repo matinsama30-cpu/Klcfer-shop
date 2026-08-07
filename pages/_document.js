8b5db31fdd2892a630871018ead8f1d59aa7107c
matinsama30-cpu <matinsama30@gmail.com>
2026-08-07 09:12:25 +0330

    Fix _document.js - remove invalid crossOrigin and style attributes

--- pages/_document.js
+++ pages/_document.js
@@ -3,13 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="fa" dir="rtl">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      </Head>
-      <body style={{ fontFamily: "'Vazir', 'Segoe UI', sans-serif" }}>
+      <Head />
+      <body>
         <Main />
         <NextScript />
       </body>
